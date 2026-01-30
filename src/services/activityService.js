c15e718b3cc2c9af7ede26874e10089123dd5479
Nick Choate <nickchoate@berkeley.edu>
2026-01-30 10:28:11 -0800

    removed some slashed code from services

--- src/services/activityService.js
+++ src/services/activityService.js
@@ -1,52 +1,3 @@
-// const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/locations/`;
-
-// const createActivity = async (locationId, activityFormData) => {
-//   try {
-//     const res = await fetch(`${BASE_URL}/${locationId}/activities`, {
-//       method: 'POST',
-//       headers: {
-//         Authorization: `Bearer ${localStorage.getItem('token')}`,
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(activityFormData),
-//     });
-//     return res.json();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// const updateActivity = async (locationId, activityId, logFormData)=> {
-//   try {
-//     const res = await fetch(`${BASE_URL}/${locationId}/activities/${activityId}`, {
-//       method: 'PUT',
-//       headers: {
-//         Authorization: `Bearer ${localStorage.getItem('token')}`,
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(logFormData),
-//     });
-//     return res.json();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// const deleteActivity = async (locationId, activityId) => {
-//   try {
-//     const res = await fetch(`${BASE_URL}/${locationId}/activities/${activityId}`, {
-//       method: 'DELETE',
-//       headers: {
-//         Authorization: `Bearer ${localStorage.getItem('token')}`,
-//       },
-//     });
-//     return res.json();
-//   } catch (error) {
-//     console.log(error);
-//   }
-
-
-
 import { request } from './apiClient.js';
 
 const BASE_PATH = '/locations';
@@ -76,9 +27,3 @@ export default {
   updateActivity,
   deleteActivity,
 };
-
-// export {
-//   createActivity,
-//   updateActivity,
-//   deleteActivity,
-// };
